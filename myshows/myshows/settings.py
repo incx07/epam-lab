e4864b15424030555bbc3287fe8dd111cccffd6e
incx07 <incx07@gmail.com>
2021-05-17 00:22:02 +0300

    set db and requirements

--- myshows/myshows/settings.py
+++ myshows/myshows/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-l7b+f58$@kgu&sy0#0^x3zd6us_-80+cc7g=y)k262q@xn-&^!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '127.0.0.1']
 
 
 # Application definition
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'myshows.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myshows',
+        'USER': 'myshowsuser',
+        'PASSWORD': 'myshowsuser',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -118,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

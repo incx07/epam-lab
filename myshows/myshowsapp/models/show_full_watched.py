ab3a0ffedc7b295955ee90c69203a2d0962d6950
incx07 <incx07@gmail.com>
2021-06-14 23:57:10 +0300

    fix docstrings for models

--- myshows/myshowsapp/models/show_full_watched.py
+++ myshows/myshowsapp/models/show_full_watched.py
@@ -1,6 +1,4 @@
-"""
-Model series from the list "Watched all"
-"""
+"""Module is for a description of a model from the list "Watched all"."""
 
 from django.db import models
 from django.conf import settings
@@ -8,7 +6,23 @@ from django.conf import settings
 
 class ShowFullWatched(models.Model):
     """
-    Class is containing the description of model fields
+    Class is containing the description of fields for "Watched all" model.
+
+    ...
+
+    Attributes
+    ----------
+    user_link : int
+        link to the user who added show
+    myshows_id : int
+        link to id in myshows database
+    title_eng : str
+        the name of the show
+    year : int
+        the show release year
+    rating : str
+        the rating that the user has set for the show
+
     """
 
     user_link = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -18,4 +32,5 @@ class ShowFullWatched(models.Model):
     rating = models.CharField(max_length=5, default='No')
 
     def __str__(self):
+        """Method returns a string representation of object."""
         return str(self.title_eng)
--- myshows/myshowsapp/models/show_to_watch.py
+++ myshows/myshowsapp/models/show_to_watch.py
@@ -1,6 +1,4 @@
-"""
-Model series from the list "Going to watch"
-"""
+"""Module is for a description of a model from the list "Going to watch"."""
 
 from django.db import models
 from django.conf import settings
@@ -8,7 +6,21 @@ from django.conf import settings
 
 class ShowToWatch(models.Model):
     """
-    Class is containing the description of model fields
+    Class is containing the description of fields for "Going to watch" model.
+
+    ...
+
+    Attributes
+    ----------
+    user_link : int
+        link to the user who added show
+    myshows_id : int
+        link to id in myshows database
+    title_eng : str
+        the name of the show
+    year : int
+        the show release year
+
     """
 
     user_link = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -17,4 +29,5 @@ class ShowToWatch(models.Model):
     year = models.PositiveSmallIntegerField(default=0)
 
     def __str__(self):
+        """Method returns a string representation of object."""
         return str(self.title_eng)

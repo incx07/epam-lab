8e137c6cdfe23f1ce9d2c670722521695113582b
incx07 <incx07@gmail.com>
2021-07-18 20:13:29 +0300

    add some tests for API

--- myshows/myshowsapp/tests/test_later_watch_show.py
+++ myshows/myshowsapp/tests/test_later_watch_show.py
@@ -1,11 +1,13 @@
 """Creating tests for RESTful API LaterWatchShow model."""
 
+import json
 from random import randint
 from django.contrib.auth.models import User
 from rest_framework import status
-from rest_framework.test import APITestCase, APIClient
 from rest_framework.reverse import reverse
+from rest_framework.test import APIClient, APITestCase
 from myshowsapp.models import LaterWatchShow
+from myshowsapp.rest.serializers import LaterWatchDetailSerializer
 
 
 class LaterWatchShowTests(APITestCase):
@@ -29,6 +31,16 @@ class LaterWatchShowTests(APITestCase):
             title_eng = "My Show for tests",
             year = 2021
         )
+        LaterWatchShow.objects.create(
+            user_link = user_test,
+            myshows_id = 24,
+            title_eng = "My Show for tests 2",
+            year = 2020
+        )
+        self.show_excepted_data = LaterWatchDetailSerializer(self.show_test).data
+        show_queryset = LaterWatchShow.objects.all()
+        self.show_excepted_data_list = LaterWatchDetailSerializer(
+            show_queryset, many=True).data
         self.data = {
             "user_link": 12,
             "myshows_id": 1112,
@@ -42,6 +54,7 @@ class LaterWatchShowTests(APITestCase):
         url = reverse('later-watch-shows-list')
         response = self.client.get(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response['content-type'], 'application/json')
 
 
     def test_create_action_unauthorized(self):
@@ -49,6 +62,7 @@ class LaterWatchShowTests(APITestCase):
         url = reverse('later-watch-shows-list')
         response = self.client.post(url, self.data, format="json")
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response['content-type'], 'application/json')
 
 
     def test_update_action_unauthorized(self):
@@ -56,6 +70,7 @@ class LaterWatchShowTests(APITestCase):
         url = reverse('later-watch-shows-detail', kwargs={"pk": self.show_test.id})
         response = self.client.put(url, self.data, format="json")
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response['content-type'], 'application/json')
 
 
     def test_destroy_action_unauthorized(self):
@@ -63,6 +78,22 @@ class LaterWatchShowTests(APITestCase):
         url = reverse('later-watch-shows-detail', kwargs={"pk": self.show_test.id})
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response['content-type'], 'application/json')
+
+
+    def test_list_action(self):
+        """ Test GET request (action: list) from an authorized user."""
+        record_count = LaterWatchShow.objects.count()
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + self.access_token)
+        url = reverse('later-watch-shows-list')
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response['content-type'], 'application/json')
+        self.assertEqual(len(response.data), record_count)
+        self.assertEqual(
+            json.loads(response.content.decode('utf8')),
+            self.show_excepted_data_list
+            )
 
 
     def test_retrieve_action(self):
@@ -71,7 +102,12 @@ class LaterWatchShowTests(APITestCase):
         url = reverse('later-watch-shows-detail', kwargs={"pk": self.show_test.id})
         response = self.client.get(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.json().get("title_eng"), self.show_test.title_eng)
+        self.assertEqual(response['content-type'], 'application/json')
+        #self.assertEqual(response.json().get("title_eng"), self.show_test.title_eng)
+        self.assertEqual(
+            json.loads(response.content.decode('utf8')),
+            self.show_excepted_data
+            )
 
 
     def test_create_action(self):
@@ -81,12 +117,14 @@ class LaterWatchShowTests(APITestCase):
         url = reverse('later-watch-shows-list')
         response = self.client.post(url, self.data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response['content-type'], 'application/json')
         self.assertEqual(LaterWatchShow.objects.count(), record_count + 1)
 
 
     def test_not_found_url(self):
-        """ Test response for not founded URL."""       
+        """ Test response for not founded URL."""
         self.client.credentials(HTTP_AUTHORIZATION='JWT ' + self.access_token)
-        url = reverse('later-watch-shows-detail', kwargs={"pk": randint(10, 100)})
+        url = reverse('later-watch-shows-detail', kwargs={"pk": randint(100, 900)})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response['content-type'], 'application/json')

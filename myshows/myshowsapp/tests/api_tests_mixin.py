305ef5460ea6a76fc8ff4e6f8537a663350c4c66
incx07 <incx07@gmail.com>
2021-07-24 21:27:39 +0300

    fix for tests

--- myshows/myshowsapp/tests/api_tests_mixin.py
+++ myshows/myshowsapp/tests/api_tests_mixin.py
@@ -12,7 +12,7 @@ class APITestsMixin():
     """Mixin class for testing API."""
     model = None
     serializer = None
-    url = None
+    base_url = None
     valid_test_data = {
         "user_link": 12,
         "myshows_id": 1112,
@@ -75,7 +75,7 @@ class APITestsMixin():
 
     def test_list_action_unauthorized(self):
         """ Test GET request from an unauthorized user."""
-        url = reverse(f'{self.url}-list')
+        url = reverse(f'{self.base_url}-list')
         response = self.client.get(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(response['content-type'], 'application/json')
@@ -83,7 +83,7 @@ class APITestsMixin():
 
     def test_create_action_unauthorized(self):
         """ Test POST request from an unauthorized user."""
-        url = reverse(f'{self.url}-list')
+        url = reverse(f'{self.base_url}-list')
         response = self.client.post(url, self.valid_test_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(response['content-type'], 'application/json')
@@ -91,7 +91,7 @@ class APITestsMixin():
 
     def test_update_action_unauthorized(self):
         """ Test UPDATE request from an unauthorized user."""
-        url = reverse(f'{self.url}-detail', kwargs={"pk": self.show_test.id})
+        url = reverse(f'{self.base_url}-detail', kwargs={"pk": self.show_test.id})
         response = self.client.put(url, self.valid_test_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(response['content-type'], 'application/json')
@@ -99,7 +99,7 @@ class APITestsMixin():
 
     def test_destroy_action_unauthorized(self):
         """ Test DELETE request from an unauthorized user."""
-        url = reverse(f'{self.url}-detail', kwargs={"pk": self.show_test.id})
+        url = reverse(f'{self.base_url}-detail', kwargs={"pk": self.show_test.id})
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(response['content-type'], 'application/json')
@@ -109,7 +109,7 @@ class APITestsMixin():
         """ Test GET request (action: list) from an authorized user."""
         objects_count = self.model.objects.filter(user_link = self.user_test_01).count()
         self.client.credentials(HTTP_AUTHORIZATION='JWT ' + self.access_token)
-        url = reverse(f'{self.url}-list')
+        url = reverse(f'{self.base_url}-list')
         response = self.client.get(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response['content-type'], 'application/json')
@@ -123,7 +123,7 @@ class APITestsMixin():
     def test_retrieve_action(self):
         """ Test GET request (action: retrieve) from an authorized user."""
         self.client.credentials(HTTP_AUTHORIZATION='JWT ' + self.access_token)
-        url = reverse(f'{self.url}-detail', kwargs={"pk": self.show_test.id})
+        url = reverse(f'{self.base_url}-detail', kwargs={"pk": self.show_test.id})
         response = self.client.get(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response['content-type'], 'application/json')
@@ -137,7 +137,7 @@ class APITestsMixin():
         """ Test POST request from an authorized user."""
         objects_count = self.model.objects.count()
         self.client.credentials(HTTP_AUTHORIZATION='JWT ' + self.access_token)
-        url = reverse(f'{self.url}-list')
+        url = reverse(f'{self.base_url}-list')
         response = self.client.post(url, self.valid_test_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response['content-type'], 'application/json')
@@ -148,7 +148,7 @@ class APITestsMixin():
         """ Test UPDATE request from an authorized user."""
         objects_count = self.model.objects.count()
         self.client.credentials(HTTP_AUTHORIZATION='JWT ' + self.access_token)
-        url = reverse(f'{self.url}-detail', kwargs={"pk": self.show_test.id})
+        url = reverse(f'{self.base_url}-detail', kwargs={"pk": self.show_test.id})
         response = self.client.put(url, self.valid_test_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response['content-type'], 'application/json')
@@ -163,10 +163,10 @@ class APITestsMixin():
         """ Test DELETE request from an authorized user."""
         objects_count = self.model.objects.count()
         self.client.credentials(HTTP_AUTHORIZATION='JWT ' + self.access_token)
-        url = reverse(f'{self.url}-detail', kwargs={"pk": self.show_test.id})
+        url = reverse(f'{self.base_url}-detail', kwargs={"pk": self.show_test.id})
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        #self.assertEqual(response['content-type'], 'application/json')
+        self.assertEqual(response['content-length'], '0')
         self.assertEqual(self.model.objects.count(), objects_count - 1)
 
 
@@ -175,12 +175,12 @@ class APITestsMixin():
         objects_count = self.model.objects.count()
         self.client.credentials(HTTP_AUTHORIZATION='JWT ' + self.access_token)
         max_length = self.model._meta.get_field('title_eng').max_length
-        url = reverse(f'{self.url}-list')
+        url = reverse(f'{self.base_url}-list')
         response = self.client.post(url, self.invalid_long_test_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response['content-type'], 'application/json')
         self.assertEqual(
-            response.data['title_eng'][0], 
+            response.data['title_eng'][0],
             f'Ensure this field has no more than {max_length} characters.'
         )
         self.assertEqual(self.model.objects.count(), objects_count)
@@ -189,7 +189,7 @@ class APITestsMixin():
     def test_not_found_url(self):
         """ Test response for not founded URL."""
         self.client.credentials(HTTP_AUTHORIZATION='JWT ' + self.access_token)
-        url = reverse(f'{self.url}-detail', kwargs={"pk": randint(100, 900)})
+        url = reverse(f'{self.base_url}-detail', kwargs={"pk": randint(100, 900)})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertEqual(response['content-type'], 'application/json')
--- myshows/myshowsapp/tests/test_rest_api.py
+++ myshows/myshowsapp/tests/test_rest_api.py
@@ -12,7 +12,7 @@ class LaterWatchShowTests(APITestsMixin, APITestCase):
 
     model = LaterWatchShow
     serializer = LaterWatchDetailSerializer
-    url = 'later-watch-shows'
+    base_url = 'later-watch-shows'
 
 
 class FullWatchedShowTests(APITestsMixin, APITestCase):
@@ -20,7 +20,7 @@ class FullWatchedShowTests(APITestsMixin, APITestCase):
 
     model = FullWatchedShow
     serializer = FullWatchedDetailSerializer
-    url = 'full-watched-shows'
+    base_url = 'full-watched-shows'
     APITestsMixin.valid_test_data["rating"] = 4
     APITestsMixin.invalid_long_test_data["rating"] = 5
     invalid_rating_test_data = {
@@ -30,18 +30,19 @@ class FullWatchedShowTests(APITestsMixin, APITestCase):
         "year": 2003,
         "rating": 6
         }
-    
+
+
     def test_create_action_with_invalid_rating(self):
         """ Test POST request when field 'rating' has invalid choice."""
         objects_count = self.model.objects.count()
         self.client.credentials(HTTP_AUTHORIZATION='JWT ' + self.access_token)
-        url = reverse(f'{self.url}-list')
+        url = reverse(f'{self.base_url}-list')
         response = self.client.post(url, self.invalid_rating_test_data, format="json")
         invalid_rating = self.invalid_rating_test_data['rating']
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response['content-type'], 'application/json')
         self.assertEqual(
-            response.data['rating'][0], 
+            response.data['rating'][0],
             f'"{invalid_rating}" is not a valid choice.'
         )
         self.assertEqual(self.model.objects.count(), objects_count)

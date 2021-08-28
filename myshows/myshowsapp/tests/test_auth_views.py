b3f3c0007673eb7c022a53a63e6e97de96dcfd28
incx07 <incx07@gmail.com>
2021-08-29 01:50:43 +0300

    adding tests for logout and registed view

--- myshows/myshowsapp/tests/test_auth_views.py
+++ myshows/myshowsapp/tests/test_auth_views.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse
 from unittest.mock import patch
-from ..views.auth_views import LoginView
+from ..views.auth_views import LoginView, LogoutView, RegisterView
 
 
 class AuthViewsTest(SimpleTestCase):
@@ -9,16 +9,30 @@ class AuthViewsTest(SimpleTestCase):
 
     def test_get_login_page(self):
         response = self.client.get(reverse('login'))
+        self.assertEqual(response.resolver_match.func.__name__, LoginView.as_view().__name__)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'registration/login.html')
-        self.assertEqual(response.resolver_match.func.__name__, LoginView.as_view().__name__)
 
 
     @patch('myshowsapp.forms.client')
-    def test_redirect_to_index_page_after_success_login(self, mock_client):
+    def test_redirect_and_set_cookie_after_success_login(self, mock_client):
         mock_client.is_authenticated = True
         credential = {'username':'first user', 'password':'Password111'}
         response = self.client.post(reverse('login'), data=credential)
         self.assertTrue(mock_client.login.called)
+        self.assertIn('refresh_token=None; HttpOnly;', str(response.cookies))
         self.assertRedirects(response, '/', target_status_code=302)
     
+
+    def test_redirect_and_delete_cookie_after_logout(self):
+        response = self.client.get(reverse('logout'))
+        self.assertEqual(response.resolver_match.func.__name__, LogoutView.as_view().__name__)
+        self.assertIn('refresh_token="";', str(response.cookies))
+        self.assertRedirects(response, '/login/')
+
+
+    def test_get_register_page(self):
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.resolver_match.func.__name__, RegisterView.as_view().__name__)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/register.html')
